f8a6cb8c473e4babe216994749068d24130024eb
mdtauqeeralam21 <mdta2108@gmail.com>
2023-07-11 16:36:27 +0530

    get cms data

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,39 @@
-import { Inter } from 'next/font/google'
+import Homepage from '@/components/Homepage'
+import { createClient } from 'contentful'
 
-import NewsCard from '@/components/NewsCard'
+export async function getStaticProps(){
+  const client = createClient({
+    space: process.env.SPACE_ID,
+    accessToken: process.env.ACCESS_KEY,
+  })
 
-const inter = Inter({ subsets: ['latin'] })
+  const res = await client.getEntries({ content_type: "news" })
 
-export default function Home() {
+  return {
+    props: {
+      news: res.items,
+    }
+  }
+}
+
+export default function Home({ news }) {
+  console.log(news);
   return (
     <>
-    <NewsCard />
+    <h1>fetching data</h1>
+    <div className="recipe-list">
+      {news.map(item => (
+        <Homepage key={item.sys.id} news={item} />
+      ))}
+
+<style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}</style>
+    </div>
           </>
   )
 }
